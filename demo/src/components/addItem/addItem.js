367a951390254acf5cd0f9a2354ee51e715e98df
李宇登 <li@maichong.it>
2016-11-16 17:57:13 +0800

    demo重做

--- demo/src/components/addItem/addItem.js
+++ demo/src/components/addItem/addItem.js
@@ -3,3 +3,18 @@
  * @date 2016-11-16
  * @author Li <li@maichong.it>
  */
+import {Component} from 'labrador';
+import * as action from '../../utils/action';
+
+export default class AddItem extends Component {
+
+  state = { text: '' };
+
+  handleInput(e) {
+    this.setState({ text: e.detail.value });
+  }
+
+  handleTap() {
+    action.add(this.state.text);
+  }
+}
